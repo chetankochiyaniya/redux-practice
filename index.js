5575405e0d0edfe9be444b0b8266d926933997dd
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-04-07 11:43:59 +0530

    3 principal

--- index.js
+++ index.js
@@ -9,21 +9,26 @@ function reducer(state={amount:1},action){
     
         // if you write like : 
         // state.amount = state.amount + 1;  
-        // mutability - >  it will direct change current object ,and it will change previous state, so don't use it
+        // mutability - >  it will direct change current object ,and it will change previous state: [{amout:2}], next: [{amout:3},{amout:3}] it will change previous value also, so don't use it
     
         //immutability
-        // it will create copy of object
+        // it will create copy of object , in here it like : [{amout:2}], next: [{amout:2},{amout:3}] it will not chage previous value it generate copy and than store
         return {amount: state.amount + 1}
     }
     return state
 }
 
-// gobal state => check current value of state
-console.log(store.getState());
+let history=[]
 
 // subscribe method runs after every state change or reducer change or after dispatch
-store.subscribe(()=>console.log(store.getState()))
+store.subscribe(()=>
+{
+    history.push(store.getState())
+    console.log(history)
+})
 
 
+setInterval(()=>{
 // event handle 
 store.dispatch({type:'increment'})
+},5000)
