b45df554067faf6f774fd15055ffb587f44222f2
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-04-07 11:17:51 +0530

    create strore, reducer, action , dispatch and subscribe

---
+++ index.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux'
+
+// store
+const store = createStore(reducer)
+
+//reducer funation
+function reducer(state={amount:1},action){
+    if(action.type==='increment'){
+    
+        // if you write like : 
+        // state.amount = state.amount + 1;  
+        // mutability - >  it will direct change current object ,and it will change previous state, so don't use it
+    
+        //immutability
+        // it will create copy of object
+        return {amount: state.amount + 1}
+    }
+    return state
+}
+
+// gobal state => check current value of state
+console.log(store.getState());
+store.subscribe(()=>console.log(store.getState()))
+
+
+// event handle 
+store.dispatch({type:'increment'})
