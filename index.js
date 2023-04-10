4ca50bc52af4be3f7ce81e0fb5f0d089bdda52c5
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-04-10 10:29:18 +0530

    Async API call and json server

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
+/**
+ * for jason-sever , it work like rest api 
+ * npx json-server --watch db.json --port 3000
+ */
+
+import axios from 'axios'
 import { createStore ,applyMiddleware} from 'redux'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+//Async API call
+// async function getUser(){
+//     const {data} =  await axios.get(' http://localhost:3000/accounts/1')
+//     console.log("data",data);
+// }
+// getUser()
 
 //action name const 
+const init = 'init'
 const inc = 'increment'
 const dec = 'decrement'
 const incByAmount = 'incrementByAmount'
 
 //logger is just use for log that give prev state, action and next state
 // store
-const store = createStore(reducer,applyMiddleware(logger.default))
+const store = createStore(reducer,applyMiddleware(logger.default,thunk.default))
 let history=[]
 
 //reducer funation
+
+// if you write like : 
+// state.amount = state.amount + 1;  
+// mutability - >  it will direct change current object ,and it will change previous state: [{amout:2}], next: [{amout:3},{amout:3}] it will change previous value also, so don't use it
+    
+//immutability
+// it will create copy of object , in here it like : [{amout:2}], next: [{amout:2},{amout:3}] it will not chage previous value it generate copy and than store
+
 function reducer(state={amount:0},action){
-    if(action.type===inc){
-    
-        // if you write like : 
-        // state.amount = state.amount + 1;  
-        // mutability - >  it will direct change current object ,and it will change previous state: [{amout:2}], next: [{amout:3},{amout:3}] it will change previous value also, so don't use it
-    
-        //immutability
-        // it will create copy of object , in here it like : [{amout:2}], next: [{amout:2},{amout:3}] it will not chage previous value it generate copy and than store
-        return {amount: state.amount + 1}
+    switch(action.type){
+        case init: 
+        return {amount: action.payload}
+        case inc:
+            return {amount: state.amount + 1}
+        case dec:
+            return {amount: state.amount - 1}
+        case incByAmount:
+            return {amount: state.amount + action.payload}
+        default:
+            return state
     }
-    if(action.type===dec){
-        return {amount: state.amount - 1}
-    }
-    
-    if (action.type === incByAmount){
-        return {amount: state.amount + action.payload}
-    }
-    return state
 }
 
 // subscribe method runs after every state change or reducer change or after dispatch
@@ -40,8 +57,23 @@ function reducer(state={amount:0},action){
 //     console.log(history)
 // })
 
-
 // Action Creators
+// Action must be plain objects.
+function getUser(id){
+
+    return async(dispatch, getState)=>{
+        const {data} =  await axios.get(`http://localhost:3000/accounts/${id}`)
+        dispatch(initUser(data.amount))
+    }
+   
+}
+
+// Action must be plain objects.
+
+function initUser(value){
+    // Action must be plain objects.
+    return {type:init,payload:value}
+} 
 function increment(){
 return {type: inc}
 }
@@ -53,9 +85,9 @@ return {type:incByAmount,payload:value}
 }
 
 setInterval(()=>{
-// event handle 
-store.dispatch(decrement())
+store.dispatch(getUser(2))
 },5000)
 
 
 // three principal 1) store should be golbal  (only 1 store) 2) immutable state 2) reducer funaction should be pure
+
