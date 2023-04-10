19f8300e7ee0b0f233eccc7f4508c7301dcf908e
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-04-10 15:22:15 +0530

    multiple reducer and handle async function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import axios from 'axios'
-import { createStore ,applyMiddleware} from 'redux'
+import { createStore ,applyMiddleware, bindActionCreators, combineReducers} from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
@@ -16,14 +16,24 @@ import thunk from 'redux-thunk'
 // getUser()
 
 //action name const 
-const init = 'init'
-const inc = 'increment'
-const dec = 'decrement'
-const incByAmount = 'incrementByAmount'
+const init = 'account/init' // domain , model -> define schema/varibale that repsenant particular value.
+const inc = 'account/increment'
+const dec = 'account/decrement'
+const incByAmount = 'account/incrementByAmount'
+const getUserPending = 'account/getUser/pendding'
+const getUserFullfilled = 'account/getUser/fulfilled'
+const getUserRejected = 'account/getuser/rejected'
+const incBonus = "bonus/increment"
 
 //logger is just use for log that give prev state, action and next state
 // store
-const store = createStore(reducer,applyMiddleware(logger.default,thunk.default))
+const store = createStore(
+    // combine method for reducers
+    combineReducers({
+        account: accountReducer,
+        bonus: bonusReducer
+    })
+    ,applyMiddleware(logger.default,thunk.default))
 let history=[]
 
 //reducer funation
@@ -35,7 +45,7 @@ let history=[]
 //immutability
 // it will create copy of object , in here it like : [{amout:2}], next: [{amout:2},{amout:3}] it will not chage previous value it generate copy and than store
 
-function reducer(state={amount:0},action){
+function accountReducer(state={amount:0},action){
     switch(action.type){
         case init: 
         return {amount: action.payload}
@@ -45,10 +55,29 @@ function reducer(state={amount:0},action){
             return {amount: state.amount - 1}
         case incByAmount:
             return {amount: state.amount + action.payload}
+        case getUserFullfilled:
+            return { amount: action.payload, pending:false };
+        case getUserRejected:
+            return {...state, error:action.error, pending:false  };
+        case getUserPending:
+            return { ...state,pending:true };
         default:
             return state
     }
 }
+// multiple reducer's 
+function bonusReducer (state = { points:0},action){
+    switch (action.type){
+        case incByAmount:
+            if(action.payload >=100)
+                return {points: state.points + 1}
+        case incBonus:
+            return {points: state.points + 1}
+        default:
+            return state
+    }
+}
+
 
 // subscribe method runs after every state change or reducer change or after dispatch
 // store.subscribe(()=>
@@ -59,14 +88,18 @@ function reducer(state={amount:0},action){
 
 // Action Creators
 // Action must be plain objects.
-function getUser(id){
-
-    return async(dispatch, getState)=>{
-        const {data} =  await axios.get(`http://localhost:3000/accounts/${id}`)
-        dispatch(initUser(data.amount))
-    }
-   
-}
+function getUser(id) {
+    return async (dispatch, getState) => {
+      try{
+          dispatch(getAccountUserPending());
+          const { data } = await axios.get(`http://localhost:3000/accounts/${id}`);
+          dispatch(getAccountUserFulFilled(data.amount));
+      } catch(error){
+          dispatch(getAccountUserRejected(error.message));
+      }
+     
+    };
+  }
 
 // Action must be plain objects.
 
@@ -84,9 +117,27 @@ function incrementByAmount(value){
 return {type:incByAmount,payload:value}
 }
 
-setInterval(()=>{
-store.dispatch(getUser(2))
-},5000)
+function incrementBonus(value){
+    return {type:incBonus}
+    }
+
+    function getAccountUserFulFilled(value) {
+        return { type: getUserFullfilled, payload: value };
+      }
+      function getAccountUserRejected(error) {
+          return { type: getUserRejected, error: error };
+      }
+        function getAccountUserPending() {
+          return { type: getUserPending };
+      }
+      
+
+
+setTimeout(() => {
+  store.dispatch(getUser(2));
+// store.dispatch(incByAmount(200))
+// store.dispatch(incBonus());
+}, 2000);
 
 
 // three principal 1) store should be golbal  (only 1 store) 2) immutable state 2) reducer funaction should be pure
